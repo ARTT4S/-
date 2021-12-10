a07ce9553d32691e58430a72183d24293332d1bd
ARTT4S <95607182+ARTT4S@users.noreply.github.com>
2021-12-10 19:28:18 +0300

    Create write.h

---
+++ write.h
@@ -0,0 +1,9 @@
+#ifndef WRITE_H_INCLUDED
+#define WRITE_H_INCLUDED
+#include "struct.h"
+#include <fstream>
+using namespace std;
+
+void write(studentInformation student[], int quont, fstream &BD);
+
+#endif
