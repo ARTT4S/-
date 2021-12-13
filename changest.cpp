52569a8f161a93f27f6e205189ca6bdb13637d85
ARTT4S <95607182+ARTT4S@users.noreply.github.com>
2021-12-14 01:59:47 +0300

    Update changest.cpp

--- changest.cpp
+++ changest.cpp
@@ -1,5 +1,6 @@
 #include "changest.h"
 #include "write.h"
+#include "typeCheck.h"
 #include <iostream>
 #include <Windows.h>
 using namespace std;
@@ -24,74 +25,82 @@ enum {
 	engGraph
 };
 
-void changeSt(studentInformation student[]) {
-	int changeCommand = 0, numSt = 0, flag = 0;
+void changeSt(studentInformation student[], int quont) {
+	int changeCommand = 0, numSt = 0, flag = 1;
 	do { 
-		cout << "Запись о каком студенте по номеру вы хотите поменять?\n";
-		cin >> numSt;
-		numSt -= 1;
-		cout << "Какую записть вы хотите поменять?\n";
-		cout << "1)  Изменить Фамилию." << endl << "2)  Изменить имя." << endl << "3)  Изменить отчество." << endl << "4)  Изменить группу." << endl << "5)  Изменить курс." << endl << "6)  Изменить оценку по высокоуровневому программированию." << endl << "7)  Изменить оценку по теоретической информатике." << endl << "8)  Изменить оценку по математическому анализу." << endl << "9)  Изменить оценку по аналитической геометрии." << endl << "10) Изменить оценку по английскому языку." << endl << "11) Изменить оценку по инженерной графике." << endl << "0) Возврат к меню." << endl;;
-		cin >> changeCommand;
+		cout << "Вы хотите изменить запись?" << endl << "1) Да" << endl << "2) Нет\n";
+		flag = typeCheck(flag, yes, no);
 		system("cls");
-		switch (changeCommand) {
-		case ext:
-			cout << "Возвращаем вас к меню сортировки.\n";
+		switch (flag) {
+		case yes:
+			cout << "Запись о каком студенте по номеру вы хотите поменять?\n";
+			numSt = typeCheck(numSt, 1, quont);
+			numSt -= 1;
+			system("cls");
+			cout << "Что вы хотите поменять?\n";
+			cout << "0)  Возврат к меню." << endl << "1)  Изменить Фамилию." << endl << "2)  Изменить имя." << endl << "3)  Изменить отчество." << endl << "4)  Изменить группу." << endl << "5)  Изменить курс." << endl << "6)  Изменить оценку по высокоуровневому программированию." << endl << "7)  Изменить оценку по теоретической информатике." << endl << "8)  Изменить оценку по математическому анализу." << endl << "9)  Изменить оценку по аналитической геометрии." << endl << "10) Изменить оценку по английскому языку." << endl << "11) Изменить оценку по инженерной графике." << endl;
+			changeCommand = typeCheck(changeCommand, ext, engGraph);
+			system("cls");
+			switch (changeCommand) {
+			case ext:
+				cout << "Возвращаем вас к меню сортировки.\n";
+				break;
+			case surname:
+				cout << "Внесите изменения в поле Фамилия: ";
+				cin >> student[numSt].surname;
+				break;
+			case name:
+				cout << "Внесите изменения в поле Имя: ";
+				cin >> student[numSt].name;
+				break;
+			case patronymic:
+				cout << "Внесите изменения в поле Отчество: ";
+				cin >> student[numSt].patronymic;
+				break;
+			case group:
+				cout << "Внесите изменения в поле Группа: ";
+				cin >> student[numSt].group;
+				break;
+			case course:
+				cout << "Внесите изменения в поле Курс: ";
+				student[numSt].course = typeCheck(student[numSt].course, minCourse, maxCourse);
+				break;
+			case highProgr:
+				cout << "Внесите изменения в поле Выс. прог.: ";
+				student[numSt].discipline.highProgr = typeCheck(student[numSt].discipline.highProgr, minGrade, maxGrade);
+				break;
+			case teorInf:
+				cout << "Внесите изменения в поле Теор. инф.: ";
+				student[numSt].discipline.teorInf = typeCheck(student[numSt].discipline.teorInf, minGrade, maxGrade);
+				break;
+			case math:
+				cout << "Внесите изменения в поле Мат. анализ: ";
+				student[numSt].discipline.math = typeCheck(student[numSt].discipline.math, minGrade, maxGrade);
+				break;
+			case anGeom:
+				cout << "Внесите изменения в поле Аналит. геом.: ";
+				student[numSt].discipline.anGeom = typeCheck(student[numSt].discipline.anGeom, minGrade, maxGrade);
+				break;
+			case eng:
+				cout << "Внесите изменения в поле Английский: ";
+				student[numSt].discipline.english = typeCheck(student[numSt].discipline.english, minGrade, maxGrade);
+				break;
+			case engGraph:
+				cout << "Внесите изменения в поле Инж. граф.: ";
+				student[numSt].discipline.engGraph = typeCheck(student[numSt].discipline.engGraph, minGrade, maxGrade);
+				break;
+			default:
+				cout << "Введена неверная команда.\n";
+			}
 			break;
-		case surname:
-			cout << "Внесите изменения в поле Фамилия: ";
-			cin >> student[numSt].surname;
-			break;
-		case name:
-			cout << "Внесите изменения в поле Имя: ";
-			cin >> student[numSt].name;
-			break;
-		case patronymic:
-			cout << "Внесите изменения в поле Отчество: ";
-			cin >> student[numSt].patronymic;
-			break;
-		case group:
-			cout << "Внесите изменения в поле Группа: ";
-			cin >> student[numSt].group;
-			break;
-		case course:
-			cout << "Внесите изменения в поле Курс: ";
-			cin >> student[numSt].course;
-			break;
-		case highProgr:
-			cout << "Внесите изменения в поле Выс. прог.: ";
-			cin >> student[numSt].discipline.highProgr;
-			break;
-		case teorInf:
-			cout << "Внесите изменения в поле Теор. инф.: ";
-			cin >> student[numSt].discipline.teorInf;
-			break;
-		case math:
-			cout << "Внесите изменения в поле Мат. анализ: ";
-			cin >> student[numSt].discipline.math;
-			break;
-		case anGeom:
-			cout << "Внесите изменения в поле Аналит. геом.: ";
-			cin >> student[numSt].discipline.anGeom;
-			break;
-		case eng:
-			cout << "Внесите изменения в поле Английский: ";
-			cin >> student[numSt].discipline.english;
-			break;
-		case engGraph:
-			cout << "Внесите изменения в поле Инж. граф.: ";
-			cin >> student[numSt].discipline.engGraph;
+		case no:
+			cout << "Возвращаем вас к главному меню.\n";
+			system("pause");
 			break;
 		default:
 			cout << "Введена неверная команда.\n";
+			break;
 		}
-		cout << "Хотите ли вы ещё что-то изменить?" << endl << "1) Да" << endl << "2) Нет\n";
-		cin >> flag;
-		if (flag == no) {
-			cout << "Чтобы вернуться к главному меню нажмите ENTER.\n";
-			cin.get();
-			cin.get();
-			changeCommand = 0;
-		}
-	} while (changeCommand != 0);
+		system("cls");
+	} while (flag != no);
 } 
