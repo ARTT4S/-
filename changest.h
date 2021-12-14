c1ff49ad8153a0b00de3c66d73534be78be10383
ARTT4S <95607182+ARTT4S@users.noreply.github.com>
2021-12-14 21:15:09 +0300

    Update changest.h

--- changest.h
+++ changest.h
@@ -1,7 +1,8 @@
 #ifndef CHANGEST_H_INCLUDED
 #define CHANGEST_H_INCLUDED
 #include "struct.h"
+#include <fstream>
 
-void changeSt(studentInformation student[], int quont);
+void changeSt(studentInformation student[], int quont, fstream &BD);
 
 #endif
