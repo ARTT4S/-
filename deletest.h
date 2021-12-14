23f73cc94d702a5a1260b12ee026c0cd4e70db49
ARTT4S <95607182+ARTT4S@users.noreply.github.com>
2021-12-14 21:14:06 +0300

    Update deletest.h

--- deletest.h
+++ deletest.h
@@ -1,7 +1,8 @@
 #ifndef DELETEST_H_INCLUDED
 #define DELETEST_H_INCLUDED
 #include "struct.h"
+#include <fstream>
 
-int deleteSt(studentInformation student[], int quont);
+int deleteSt(studentInformation student[], int quont, fstream &BD);
 
 #endif
