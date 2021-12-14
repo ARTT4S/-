8c962950f8905efc7818643db6d1d726bf71294a
ARTT4S <95607182+ARTT4S@users.noreply.github.com>
2021-12-14 20:05:45 +0300

    Update deletest.cpp

--- deletest.cpp
+++ deletest.cpp
@@ -11,14 +11,14 @@ enum {
 int deleteSt(studentInformation student[], int quont) {
 	int flag = 0, delstr = 0, copyi = 0;
 	while (flag != no) {
-		cout << "Вы хотите удалить запись?" << endl << "1) Да" << endl << "2) Нет\n";
+		cout << "Какую по счёту запись вы хотите удалить?\n";
+		delstr = typeCheck(delstr, 1, quont);
+		system("cls");
+		cout << "Вы точно хотите удалить запись?" << endl << "1) Да" << endl << "2) Нет\n";
 		flag = typeCheck(flag, yes, no);
 		system("cls");
 		switch (flag) {
 		case yes:
-			cout << "Какую по счёту запись вы хотите удалить?\n";
-			delstr = typeCheck(delstr, 1, quont);
-			system("cls");
 			delstr -= 1;
 			for (int j = 0; j < quont; ++j) {
 				if (j != delstr) {
